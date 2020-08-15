408309ea3a5bffb5b7afa8b2d3e113ef48be2090
Antonio Guedes <gguedesantonio@gmail.com>
2020-08-15 23:18:44 +1000

    Schema fixed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burgers_name) VALUES ('Aussie Burger');
-INSERT INTO burgers (burgers_name) VALUES ('Cheeseburger');
-INSERT INTO burgers (burgers_name) VALUES ('BBQ Burger');
+INSERT INTO burgers (burger_name) VALUES ('Aussie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Burger');
