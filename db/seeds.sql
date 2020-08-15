4b5015a5397e339cbcd50bdb91e3d8684c929bef
Antonio Guedes <gguedesantonio@gmail.com>
2020-08-15 23:07:59 +1000

    Seed updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,3 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (burgers_name) VALUES ('Aussie Burger');
+INSERT INTO burgers (burgers_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burgers_name) VALUES ('BBQ Burger');
