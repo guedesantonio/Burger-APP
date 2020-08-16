ba8b51602debcb1d9f39613f383c44b31a28fb95
Antonio Guedes <gguedesantonio@gmail.com>
2020-08-16 17:50:20 +1000

    Fixed typo on controller

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -3,11 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-const cat = require("../models/cat.js");
+const burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", (req, res) => {
-  cat.all((data) => {
+  burger.selectAll((data) => {
     const hbsObject = {
       cats: data
     };
@@ -16,39 +16,26 @@ router.get("/", (req, res) => {
   });
 });
 
-router.post("/api/cats", (req, res) => {
-  cat.create([
-    "name", "sleepy"
+router.post("/api/burger", (req, res) => {
+  burger.insertOne([
+    "name"
   ], [
-    req.body.name, req.body.sleepy
+    req.body.name
   ], (result) => {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/cats/:id", (req, res) => {
+router.put("/api/burgers/:id", (req, res) => {
   const condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  cat.update({
-    sleepy: req.body.sleepy
+  burger.updateOne({
+    devoured: req.body.devoured
   }, condition, (result) => {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
-router.delete("/api/cats/:id", (req, res) => {
-  const condition = "id = " + req.params.id;
-
-  cat.delete(condition, (result) => {
-    if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
