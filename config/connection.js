9e20ee1f66480b7a53521304bf24375c90f5ab2c
Antonio Guedes <gguedesantonio@gmail.com>
2020-08-17 22:04:33 +1000

    JAWS sintax fixed again

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,20 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-const connection;
+let connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "gege101101",
-  database: "burger_db"
-});
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "gege101101",
+    database: "burger_db"
+  });
 }
-
 // Make connection.
 connection.connect((err) => {
   if (err) {
